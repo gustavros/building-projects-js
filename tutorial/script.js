9cf079e5db9284cfed0bb06254fd974744eb3271
Gustavo <gustavossw@hotmail.com>
2022-03-18 09:52:21 -0300

    color flipper done

--- tutorial/script.js
+++ tutorial/script.js
@@ -1,13 +1,13 @@
 
 let hello = function msg() {
-    alert('Opa!')
+        alert('Opa!')
 }
 
 let squad = document.getElementById('quadrado')
 squad.addEventListener('click', hello)
 
 function changeColor(squad, color) {
-    squad.style.backgroundColor = color;
+        squad.style.backgroundColor = color;
 
 }
 
