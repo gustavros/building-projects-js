1f917a6e76c3a56cebf9dfc709c307d4435b1d74
Gustavo <gustavossw@hotmail.com>
2022-03-24 11:36:55 -0300

    a

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -8,10 +8,10 @@ btn.addEventListener('click', function() {
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
 
-    document.body.style.backgroundColor = colors[randomNumber];
+document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
+      return Math.floor(Math.random() * colors.length);
 }
